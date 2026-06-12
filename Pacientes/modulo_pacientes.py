f0b684f18cd230636a8488e77a1bab095a1d964f
Jod-dever <joaoaugustospyke@gmail.com>
2026-06-11 21:44:48 -0300

    .

--- Pacientes/modulo_pacientes.py
+++ Pacientes/modulo_pacientes.py
@@ -78,26 +78,42 @@ def modulopacientes(pacientes):
             
             if nome in pacientes:
                 print('Nome:', nome.title())
+                print()
                 print('Telefone:', pacientes[nome]['telefone'])
+                print()
                 print('Email:', pacientes[nome]['email'])
+                print()
                 print('Data de nascimento:', pacientes[nome]['data_nascimento'])
+                print()
                 print('idade:', pacientes[nome]['idade'])
+                print()
                 print('altura:', pacientes[nome]['altura'])
+                print()
                 print('Peso:', pacientes[nome]['peso'])
-                print('Gênero:', pacientes[nome]['sexo'])
+                print()
+                print('Gênero:', pacientes[nome]['sexo'].title())
+                print()
                 print('Circunferência do pescoço:', pacientes[nome]['circ_pesc'])
+                print()
                 print('Circunferência da cintura:', pacientes[nome]['circ_cint'])
+                print()
                 print('Circunferência do quadril:', pacientes[nome]['circ_quad'])
+                print()
                 if pacientes[nome]['nivel_atv'] == '1':
                     print('Sedentário')
+                    print()
                 elif pacientes[nome]['nivel_atv'] == '2':
                     print('Levemente ativo')
+                    print()
                 elif pacientes[nome]['nivel_atv'] == '3':
                     print('Moderadamente ativo')
+                    print()
                 elif pacientes[nome]['nivel_atv'] == '4':
                     print('Muito ativo')
+                    print()
                 elif pacientes[nome]['nivel_atv'] == '5':
-                    print('Extremamente ativo')               
+                    print('Extremamente ativo')
+                    print()               
                 print('Objetivo:', pacientes[nome]['objetivo'])
                 print()
             else:
@@ -115,13 +131,18 @@ def modulopacientes(pacientes):
                     print('####################################################')
                     print('##  O que você quer atualizar?            ##########')
                     print('##  Selecione uma das opções a seguir ##############')
-                    print('##  1 - Nome do paciente                          ##')
-                    print('##  2 - Telefone do paciente                      ##')
-                    print('##  3 - Email do paciente                         ##')
-                    print('##  4 - Data de nascimento do paciente            ##')
-                    print('##  5 - Peso do paciente                          ##')
-                    print('##  6 - Objetivo do paciente                      ##')
-                    print('##  0 - Voltar ao menu dos pacientes              ##')
+                    print('## 1  -> Nome do paciente                          ##')
+                    print('## 2  -> Telefone do paciente                      ##')
+                    print('## 3  -> Email do paciente                         ##')
+                    print('## 4  -> Data de nascimento do paciente            ##')
+                    print('## 5  -> Altura do paciente                        ##')
+                    print('## 6  -> Peso do paciente                          ##')
+                    print('## 7  -> Circunferência do pescoço                 ##')
+                    print('## 8  -> Circunferência do cintura                 ##')
+                    print('## 9  -> Circunferência do quadril                 ##')
+                    print('## 10 -> Nível de atividade física                 ##')
+                    print('## 11 -> Objetivo do paciente                      ##')
+                    print('## 0  -> Voltar ao menu dos pacientes              ##')
                     resposta_pac_at = input('## Escolha a sua opção: ')
                     print()
 
@@ -130,31 +151,73 @@ def modulopacientes(pacientes):
                         pacientes[novo_nome] = pacientes[nome]
                         del pacientes[nome]
                         nome = novo_nome
+                        print()
                         print('Nome do paciente atualizado com sucesso!')
                         print()
                   
                     elif resposta_pac_at == '2':
                         pacientes[nome]['telefone'] = input('Novo telefone: ')
+                        print()
                         print('Telefone do paciente atualizado com sucesso!')
                         print()
                   
                     elif resposta_pac_at == '3':
                         pacientes[nome]['email'] = input('Novo Email: ')
+                        print()
                         print('Email do paciente atualizado com sucesso!')
                         print()
                   
                     elif resposta_pac_at == '4':
                         pacientes[nome]['data_nascimento'] = input('Nova data de nascimento: ')
+                        print()
                         print('Data de nascimento do paciente atualizado com sucesso!')
                         print()
                   
                     elif resposta_pac_at == '5':
+                        pacientes[nome]['altura'] = input('Nova altura: ')
+                        print()
+                        print('Altura do paciente atualizado com sucesso!')
+                        print()
+                    
+                    elif resposta_pac_at == '6':
                         pacientes[nome]['peso'] = input('Novo peso: ')
+                        print()
                         print('Peso do paciente atualizado com sucesso!')
                         print()
+
+                    elif resposta_pac_at == '7':
+                        pacientes[nome]['circ_pesc'] = input('Nova circunferência do pescoço: ')
+                        print()
+                        print('Circunferência do pescoço do paciente atualizada com sucesso!')
+                        print()
+
+                    elif resposta_pac_at == '8':
+                        pacientes[nome]['circ_cint'] = input('Nova circunferência da cintura: ')
+                        print()
+                        print('Circunferência da cintura do paciente atualizada com sucesso!')
+                        print()
+
+                    elif resposta_pac_at == '9':
+                        pacientes[nome]['circ_quad'] = input('Nova circunferência do quadril: ')
+                        print()
+                        print('Circunferência do quadril do paciente atualizada com sucesso!')
+                        print()
                   
-                    elif resposta_pac_at == '6':
-                        pacientes[nome]['objetivo'] = input('Novo objetivo: ')
+                    elif resposta_pac_at == '10':
+                        print('### qual o nível de atividade física do paciente ###')
+                        print('##  1 - Sedentário -> pouco ou nenhum exercício                                   ##')
+                        print('##  2 - Levemente ativo -> exercício leve ou atividade leve diária                ##')
+                        print('##  3 - Moderadamente ativo -> exercício moderado ou atividade moderada diária    ##')
+                        print('##  4 - Muito ativo -> exercício intenso ou atividade intensa diária              ##')
+                        print('##  5 - Extremamente ativo -> exercício muito intenso ou trabalho físico pesado   ##')
+                        pacientes[nome]['nivel_atv'] = input('Novo nível de atividade física: ')
+                        print()
+                        print('Nível de atividade física do paciente atualizado com sucesso!')
+                        print()
+
+                    elif resposta_pac_at == '11':
+                        pacientes[nome]['objetivo'] = input('Novo objetivo do paciente: ')
+                        print()
                         print('Objetivo do paciente atualizado com sucesso!')
                         print()
                   
