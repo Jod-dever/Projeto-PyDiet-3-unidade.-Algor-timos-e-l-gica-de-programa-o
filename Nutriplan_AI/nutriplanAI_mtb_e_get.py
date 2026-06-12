eafc34f222f2c451f0c56b874fb7c8b2994c6da9
Jod-dever <joaoaugustospyke@gmail.com>
2026-06-11 23:49:56 -0300

    .

--- Nutriplan_AI/nutriplanAI_mtb_e_get.py
+++ Nutriplan_AI/nutriplanAI_mtb_e_get.py
@@ -15,79 +15,83 @@ def mtb_get (pacientes):
             nome = input('Qual o nome do paciente? ').lower()
             print()
               
-            if pacientes[nome]['sexo'] == 'masculino':
-                tmb = (float(pacientes[nome]['peso']) * 13.75) + (float(pacientes[nome]['altura']) * 500.3) - (int(pacientes[nome]['idade']) * 6.75) + 66.5
-                if pacientes[nome]['nivel_atv'] == '1':
-                    get = tmb * 1.2
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de pouco ou nenhum exercício,')
-                    print('o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()
-                
-                elif pacientes[nome]['nivel_atv'] == '2':
-                    get = tmb * 1.375
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de exercício leve ou')
-                    print('atividade leve diária, o seu gasto energético total é de {:.2f}.'.format(get))
-                    print() 
+            if nome in pacientes:            
+                if pacientes[nome]['sexo'] == 'masculino':
+                    tmb = (float(pacientes[nome]['peso']) * 13.75) + (float(pacientes[nome]['altura']) * 500.3) - (int(pacientes[nome]['idade']) * 6.75) + 66.5
+                    if pacientes[nome]['nivel_atv'] == '1':
+                        get = tmb * 1.2
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de pouco ou nenhum exercício,')
+                        print('o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()
+                    
+                    elif pacientes[nome]['nivel_atv'] == '2':
+                        get = tmb * 1.375
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de exercício leve ou')
+                        print('atividade leve diária, o seu gasto energético total é de {:.2f}.'.format(get))
+                        print() 
 
-                elif pacientes[nome]['nivel_atv'] == '3':
-                    get = tmb * 1.55
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de exercício moderado ou')
-                    print('atividade moderada diária, o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()    
+                    elif pacientes[nome]['nivel_atv'] == '3':
+                        get = tmb * 1.55
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de exercício moderado ou')
+                        print('atividade moderada diária, o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()    
 
-                elif pacientes[nome]['nivel_atv'] == '4':
-                    get = tmb * 1.725
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de exercício intenso ou')
-                    print('atividade intensa diária, o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()
+                    elif pacientes[nome]['nivel_atv'] == '4':
+                        get = tmb * 1.725
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de exercício intenso ou')
+                        print('atividade intensa diária, o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()
 
-                elif pacientes[nome]['nivel_atv'] == '5':
-                    get = tmb * 1.9
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de exercício muito intenso ou')
-                    print('trabalho físico pesado diário, o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()
-                        
-            elif pacientes[nome]['sexo'] == 'feminino':
-                tmb = (float(pacientes[nome]['peso']) * 9.563) + (float(pacientes[nome]['altura']) * 185) - (int(pacientes[nome]['idade']) * 4.676) + 665.1
-                if pacientes[nome]['nivel_atv'] == '1':
-                    get = tmb * 1.2
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de pouco ou nenhum exercício,')
-                    print('o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()
-                        
-                elif pacientes[nome]['nivel_atv'] == '2':
-                    get = tmb * 1.375
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de exercício leve ou')
-                    print('atividade leve diária, o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()
-                        
-                elif pacientes[nome]['nivel_atv'] == '3':
-                    get = tmb * 1.55
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de exercício moderado ou')
-                    print('atividade moderada diária, o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()    
+                    elif pacientes[nome]['nivel_atv'] == '5':
+                        get = tmb * 1.9
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de exercício muito intenso ou')
+                        print('trabalho físico pesado diário, o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()
+                            
+                elif pacientes[nome]['sexo'] == 'feminino':
+                    tmb = (float(pacientes[nome]['peso']) * 9.563) + (float(pacientes[nome]['altura']) * 185) - (int(pacientes[nome]['idade']) * 4.676) + 665.1
+                    if pacientes[nome]['nivel_atv'] == '1':
+                        get = tmb * 1.2
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de pouco ou nenhum exercício,')
+                        print('o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()
+                            
+                    elif pacientes[nome]['nivel_atv'] == '2':
+                        get = tmb * 1.375
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de exercício leve ou')
+                        print('atividade leve diária, o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()
+                            
+                    elif pacientes[nome]['nivel_atv'] == '3':
+                        get = tmb * 1.55
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de exercício moderado ou')
+                        print('atividade moderada diária, o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()    
 
-                elif pacientes[nome]['nivel_atv'] == '4':
-                    get = tmb * 1.725
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de exercício intenso ou')
-                    print('atividade intensa diária, o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()
-                
-                elif pacientes[nome]['nivel_atv'] == '5':
-                    get = tmb * 1.9
-                    print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
-                    print('o nível de atividade física dele é de exercício muito intenso ou')
-                    print('trabalho físico pesado diário, o seu gasto energético total é de {:.2f}.'.format(get))
-                    print()
+                    elif pacientes[nome]['nivel_atv'] == '4':
+                        get = tmb * 1.725
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de exercício intenso ou')
+                        print('atividade intensa diária, o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()
+                    
+                    elif pacientes[nome]['nivel_atv'] == '5':
+                        get = tmb * 1.9
+                        print('A taxa de metabolismo basal do paciente {} é de {:.2f} e já que '.format(nome,tmb))
+                        print('o nível de atividade física dele é de exercício muito intenso ou')
+                        print('trabalho físico pesado diário, o seu gasto energético total é de {:.2f}.'.format(get))
+                        print()
+
+            else:
+                print('Paciente não encontrado!')    
 
         elif resposta_mtb == '2':
             idade = int(input('Digite a idade do paciente: '))
