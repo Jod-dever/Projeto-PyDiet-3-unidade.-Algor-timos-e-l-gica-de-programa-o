69e426e59b9642c15197538e2a025e8f051ce461
Jod-dever <joaoaugustospyke@gmail.com>
2026-06-12 19:42:11 -0300

    .

---
+++ Nutriplan_AI/nutriplanAI_plan_dieta.py
@@ -0,0 +1,19 @@
+def plan_dieta (pacientes):
+    
+    ### Vou precisar de get
+
+    
+    resposta_plan_dieta == ''
+    while resposta_plan_dieta != '0':
+        print('##############################################################################')
+        print('##  O que você quer fazer?                                                  ##')
+        print('##  Selecione uma das opções a seguir                                       ##')
+        print('##  1 - Cálculo para paciente já cadastrado                                 ##')
+        print('##  2 - Consulta rápida                                                     ##')
+        print('##  0 - Voltar ao menu do Nutriplan AI                                      ##')
+        resposta_plan_dieta = input('## Escolha a sua opcão: ')
+        print()
+
+
+
+
